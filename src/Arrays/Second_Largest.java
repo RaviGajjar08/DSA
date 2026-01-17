d0e6b79e808848b854986222db3f5a72249b4e61
Ravi Dharaiya <146757250+RaviGajjar08@users.noreply.github.com>
2026-01-17 16:22:19 +0530

    Second largest element

---
+++ src/Arrays/Second_Largest.java
@@ -0,0 +1,24 @@
+package Arrays;
+
+public class Second_Largest {
+    public static void  main(String[] args) {
+        int[] arr = {12, 35, 1, 10, 34, 1};
+        int firstLargest = Integer.MIN_VALUE;
+        int secondLargest = Integer.MIN_VALUE;
+
+        for (int num : arr) {
+            if (num > firstLargest) {
+                secondLargest = firstLargest;
+                firstLargest = num;
+            } else if (num > secondLargest && num != firstLargest) {
+                secondLargest = num;
+            }
+        }
+
+        if (secondLargest == Integer.MIN_VALUE) {
+            System.out.println("There is no second largest element.");
+        } else {
+            System.out.println("The second largest element is: " + secondLargest);
+        }
+    }
+}
